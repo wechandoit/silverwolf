debdb2cfe0a8b14414e8768675c5704cbd9f2a5e
wechandoit <ethanchan7981@gmail.com>
2025-06-03 19:55:29 -0400

    Add verbose stats

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, select
+from sqlalchemy import Integer, String, ARRAY, select
 from sqlalchemy.orm import Mapped, mapped_column
 import val
 
@@ -19,6 +19,14 @@ class Player(db.Model):
     puuid: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str]
     tag: Mapped[str]
+    region: Mapped[str]
+
+class Verbose_Player(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    puuid: Mapped[str] = mapped_column(unique=True)
+    account_level: Mapped[int] = mapped_column(default=0)
+    card: Mapped[str]
+    title: Mapped[str]
 
 @app.route("/")
 def hello_world():
@@ -37,7 +45,8 @@ async def get_player_stats(name, tag):
         new_player = Player(
             name=player_info['name'],
             tag=player_info['tag'],
-            puuid=player_info['puuid']
+            puuid=player_info['puuid'],
+            region=player_info['region']
         )
         db.session.add(new_player)
         db.session.commit()
@@ -46,5 +55,45 @@ async def get_player_stats(name, tag):
         return {
             'name': existing_player.name,
             'tag': existing_player.tag,
-            'puuid': existing_player.puuid
+            'puuid': existing_player.puuid,
+            'region': existing_player.region
         }
+
+@app.route("/by-puuid/<puuid>")
+async def get_verbose_player_stats(puuid):
+    is_player_in_basic_table_query = select(Player).where(
+        Player.puuid == puuid
+    )
+
+    existing_player = db.session.execute(is_player_in_basic_table_query).scalar_one_or_none()
+    if existing_player is None:
+        return f'<p>{puuid} not in the database</p>'
+    else:
+        is_player_in_verbose_table_query = select(Verbose_Player).where(
+            Verbose_Player.puuid == puuid
+        )
+
+        existing_verbose_player = db.session.execute(is_player_in_verbose_table_query).scalar_one_or_none()
+        if existing_verbose_player is None:
+            player_info = await val.get_verbose_player_stats(puuid)
+            new_player = Verbose_Player(
+                puuid=player_info['puuid'],
+                account_level=player_info['account_level'],
+                card=player_info['card'],
+                title=player_info['title']
+            )
+            db.session.add(new_player)
+            db.session.commit()
+            return {
+                'puuid': player_info['puuid'],
+                'account_level': player_info['account_level'],
+                'card': val.get_player_card(player_info['card']!=None, player_info['card']),
+                'title': await val.get_title(player_info['title']!=None, player_info['title'])
+            }
+        else:
+            return {
+                'puuid': existing_verbose_player.puuid,
+                'account_level': existing_verbose_player.account_level,
+                'card': val.get_player_card(existing_verbose_player.card!=None, existing_verbose_player.card),
+                'title': await val.get_title(existing_verbose_player.title!=None, existing_verbose_player.title)
+            }
--- backend/val.py
+++ backend/val.py
@@ -11,6 +11,8 @@ headers = {
     "Authorization": f"{API_KEY}"
 }
 
+# Get basic player stats (name, tag, puuid, region)
+
 async def get_player_stats(name, tag):
     account_data_url = f'https://api.henrikdev.xyz/valorant/v2/account/{name}/{tag}?force=true'
     async with request('GET', account_data_url, headers=headers) as response:
@@ -18,8 +20,47 @@ async def get_player_stats(name, tag):
             data = await response.json()
             data = data['data']
             
-            player_info = {'name': data['name'], 'tag': data['tag'], 'puuid': data['puuid']}
+            player_info = {'name': data['name'], 'tag': data['tag'], 'puuid': data['puuid'], 'region': data['region'].upper()}
             return player_info
 
         else:
             return f'Error fetching player stats | {response.status}: {response.reason}'
+
+# Get verbose player stats (name, tag, puuid, region, account_level, card, title)
+
+async def get_verbose_player_stats(puuid):
+    account_data_url = f'https://api.henrikdev.xyz/valorant/v2/by-puuid/account/{puuid}?force=true'
+    async with request('GET', account_data_url, headers=headers) as response:
+        if response.status == 200:
+            data = await response.json()
+            data = data['data']
+            
+            player_info = {'name': data['name'], 'tag': data['tag'], 'puuid': data['puuid'], 'region': data['region'].upper(), 
+                           'account_level': data['account_level'], 'card': data['card'], 'title': data['title']}
+            return player_info
+
+        else:
+            return f'Error fetching player stats | {response.status}: {response.reason}'
+
+# Get the player's card (small/pfp) image if it exists
+
+def get_player_card(has_card, card) -> str:
+    if has_card:
+        return f'https://media.valorant-api.com/playercards/{card}/smallart.png'
+    else:
+        return 'None'
+    
+# Get the player's title if it exists
+
+async def get_title(has_title, title) -> str:
+    if has_title:
+        title_link_url = f'https://valorant-api.com/v1/playertitles/{title}'
+        async with request('GET', title_link_url, headers={}) as response:
+            if response.status == 200:
+                data = await response.json()
+                data = data['data']
+                return data['titleText']
+            else:
+                return 'None'
+    else:
+        return 'None'
